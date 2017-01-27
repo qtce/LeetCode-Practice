5cae337b1f6997c05ac29f98969432a85b5df7d3
Bruce Lou <brucelouyt@gmail.com>
2017-01-27 23:22:29 +0800

    some change

--- ex-021.py
+++ ex-021.py
@@ -14,34 +14,16 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        res = l1
-        c1 = l1
-        c2 = l2
-        if l1 == None and l2 == None:
-            return []
-        elif l1 == None:
-            return l2
-        else:
-            return l1
+        res = cur = ListNode(0)
 
-        temp1 = c1.next
-        temp2 = c2.next
-
-        while c1.next != None or c2.next != None:
-            if c1.val <= c2.val and c1.next.val > c2.val:
-                temp1 = c1.next
-                temp2 = c2.next
-                c1.next = c2
-                c1.next.next = temp1
-                c1 = temp1
-                c2 = temp2
-            elif c1.val < c2.val and c1.next.val <= c2.val:
-                c1 = c1.next
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
             else:
-                temp2 = c2.next
-                temp1 = c1
-                c1 = c2
-                c1.next = temp1
-                c2 = temp2
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
 
-        return c1
+        return res.next
